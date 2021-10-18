49bf6fa18c84f74e83d15de3bf2ad2a896d35bf6
Arnold Grosberg <arnold.grosberg@khk.ee>
2021-10-18 09:05:12 +0300

    deleting tasks

--- app.js
+++ app.js
@@ -19,3 +19,32 @@ function addTask(e){
 	ls.addTask(task);
 	e.preventDefault();
 }
+
+
+
+// event elements
+const taskList = document.querySelector('ul');
+const delAllBtn = document.getElementById('del-tasks');
+
+// click element kustutamiseks
+taskList.addEventListener('click', deleteTask);
+
+delAllBtn.addEventListener('click', deleteTasks);
+
+function deleteTask(e) {
+if(e.target.textContent = 'X'){
+    if(confirm('Are you sure to delete this task?')){
+      e.target.parentElement.remove();
+    }
+  }
+}
+
+function deleteTasks(e) {
+	if(confirm('Are you sure to delete all tasks?')){
+      while(taskList.firstChild){
+      	taskList.removeChild(taskList.firstChild);
+// taskList.innerHTML = '';
+
+  }
+}
+}
